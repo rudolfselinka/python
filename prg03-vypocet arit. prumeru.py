eb4fc33c59f5b89573382f75b95d3e5d16c4e883
rudolfselinka <147338304+rudolfselinka@users.noreply.github.com>
2023-11-27 17:05:30 +0100

    prg03-vypocet arit. prumeru.py

--- prg03-vypocet arit. prumeru.py
+++ prg03-vypocet arit. prumeru.py
@@ -1,16 +1,8 @@
 známky = [[3, 1], [3, 2], [3, 1], [10, 3], [4, 4]]
-
-# Inicializace proměnných pro celkovou váhu a vážený součet
 celková_váha = 0
 vážený_součet = 0
-
-# Procházení každé dvojice váha, známka v seznamu
 for váha, známka in známky:
-    celková_váha += váha  # Přičítání váhy k celkové váze
-    vážený_součet += váha * známka  # Přičítání váženého součtu
-
-# Výpočet aritmetického průměru
+    celková_váha += váha 
+    vážený_součet += váha * známka  
 aritmetický_průměr = vážený_součet / celková_váha
-
-# Výpis výsledku
 print("Aritmetický průměr:", aritmetický_průměr)
