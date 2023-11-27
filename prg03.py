66f16a78c5e1b35a5a73111cf4db395d4fc2f686
rudolfselinka <147338304+rudolfselinka@users.noreply.github.com>
2023-11-27 17:04:33 +0100

    Delete prg03.py

--- prg03.py
+++
@@ -1,8 +0,0 @@
-známky = [[3, 1], [3, 2], [3, 1], [10, 3], [4, 4]]
-celková_váha = 0
-vážený_součet = 0
-for váha, známka in známky:
-    celková_váha += váha  
-    vážený_součet += váha * známka  
-aritmetický_průměr = vážený_součet / celková_váha
-print("Aritmetický průměr:", aritmetický_průměr)
