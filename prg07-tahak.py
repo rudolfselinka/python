7269661a75211c9244189baa78ddc99a4d4f9408
rudolfselinka <147338304+rudolfselinka@users.noreply.github.com>
2023-12-04 21:28:25 +0100

    Add files via upload

---
+++ prg07-tahak.py
@@ -0,0 +1,33 @@
+#print - Vytiskne text
+print=("Hello World!")
+#input - Umožní uživateli, aby zadal data, s kterými program pracuje
+input()
+#if - Jestliže
+x=1
+y=2
+if x > y:
+    print(x)
+#else - Jinak
+else:
+    print(y)
+#range - Rozmezí
+for i in range(10):
+    print(i)
+#round - Zaokrouhlení
+round(5.512)
+#len - Délka(počet něčeho)
+n=[1,2,3,4]
+len(n)
+print(len)
+#import - Vložení knihovny do programu
+import turtle
+#tmp -  Prohození hodnot ve dvouch proměnných
+a=1
+b=2
+tmp=a
+a=b
+b=tmp
+#reverse - Obrácení pole
+ovoce = ['jablko', 'banán', 'třešeň,']
+ovoce.reverse()
+print(ovoce)
