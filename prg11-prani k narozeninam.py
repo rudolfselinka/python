e50b45790257d0dc610e71abae4734bcc6adcd31
rudolfselinka <147338304+rudolfselinka@users.noreply.github.com>
2023-12-18 16:49:41 +0100

    Add files via upload

---
+++ prg11-prani k narozeninam.py
@@ -0,0 +1,9 @@
+def narozeniny(x, y):
+    print(f"Vsechno nejlepsi {x}!")
+    print(f"Je ti {y} let!")
+    print("Hodne stesti a zdravi!")
+    print()
+
+narozeniny("Michale", 16)
+narozeniny("Petro", 24)
+narozeniny("Ondreji", 13)
